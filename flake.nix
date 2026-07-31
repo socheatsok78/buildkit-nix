86ef68e5ce20934dedded6e094c3ae280d11ab56
Socheat <github@socheat.dev>
2026-07-31 20:03:54 +0700

    Initialize project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          # It is recommended to pin Go version to avoid issues with breaking changes in the future.
+          # You can uncomment the following lines to pin a specific version of Go and its tools.
+          # go = pkgs.go_1_25;
+          # gotools = pkgs.gotools.override { go = go; };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              delve
+              go
+              go-tools
+              gopls
+              gotools
+            ];
+          };
+        }
+      );
+    };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/socheatsok78/buildkit-nix
+
+go 1.26.5
