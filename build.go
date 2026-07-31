aaabfa0d27f4f2394a0ab946e1ede6921c9a5915
Socheat <github@socheat.dev>
2026-08-01 00:18:09 +0700

    Revert "[DRAFT]: build ref"
    This reverts commit fe47a8375e5a9095b2cd36f421bee79034f42442.

--- build.go
+++ build.go
@@ -43,14 +43,11 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 	// Load the nix image and set it as the base image for the build
 	builder := llb.Image(NixImage, llb.WithMetaResolver(c), dockerui.WithInternalName(fmt.Sprintf("load builder image from %s", NixImage)))
 
-	// builder = builder.Security(llb.SecurityModeSandbox)
-
 	// Run the nix build command inside the nix image
 	builderSt := builder.Dir("/workspace").Run(
 		llb.AddMount("/src", src),
 		llb.Args([]string{
 			"nix",
-			"--option", "sandbox", "true",
 			"--extra-experimental-features", "nix-command",
 			"--extra-experimental-features", "flakes",
 			"build",
@@ -73,62 +70,58 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	builderRes, err := c.Solve(ctx, client.SolveRequest{
 		Definition: builderDef.ToPB(),
 	})
 	if err != nil {
 		return nil, err
 	}
+	builderRef, err := builderRes.SingleRef()
+	if err != nil {
+		return nil, err
+	}
 
-	builderRes.EachRef(func(r client.Reference) error {
-		builderRef := r
+	// Parse the manifest.json file to get the list of layers and the config file
+	manifestByte, err := builderRef.ReadFile(ctx, client.ReadRequest{Filename: "/manifest.json"})
+	if err != nil {
+		return nil, err
+	}
+	manifest, err := dockershim.UnmarshalManifest(manifestByte)
+	if err != nil {
+		return nil, err
+	}
 
-		// Parse the manifest.json file to get the list of layers and the config file
-		manifestByte, err := builderRef.ReadFile(ctx, client.ReadRequest{Filename: "/manifest.json"})
-		if err != nil {
-			return err
-		}
-		manifest, err := dockershim.UnmarshalManifest(manifestByte)
-		if err != nil {
-			return err
-		}
+	// Create a new scratch state to hold the extracted layers
+	st := llb.Scratch()
 
-		// Create a new scratch state to hold the extracted layers
-		st := llb.Scratch()
+	// Import each layer from the result of the nix build into the new scratch state
+	for _, layer := range manifest.Layers {
+		st = st.File(
+			llb.Copy(layeredSt, layer, "/", &llb.CopyInfo{
+				AttemptUnpack: true,
+			}),
+			nixui.WithInternalName(fmt.Sprintf("importing layer: %s", layer)),
+		)
+	}
 
-		// Import each layer from the result of the nix build into the new scratch state
-		for _, layer := range manifest.Layers {
-			st = st.File(
-				llb.Copy(layeredSt, layer, "/", &llb.CopyInfo{
-					AttemptUnpack: true,
-				}),
-				nixui.WithInternalName(fmt.Sprintf("importing layer: %s", layer)),
-			)
-		}
-
-		// Prepare the final definition from final state
-		def, err := st.Marshal(ctx)
-		if err != nil {
-			return err
-		}
-		res, err := c.Solve(ctx, client.SolveRequest{
-			Definition: def.ToPB(),
-		})
-		if err != nil {
-			return err
-		}
-
-		// Read the config file from the result of the nix build and add it to the result metadata
-		configByte, err := builderRef.ReadFile(ctx, client.ReadRequest{Filename: "/" + manifest.Config})
-		if err != nil {
-			return err
-		}
-
-		res.AddMeta(exptypes.ExporterImageConfigKey, configByte)
-
-		return nil
+	// Prepare the final definition from final state
+	def, err := st.Marshal(ctx)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.Solve(ctx, client.SolveRequest{
+		Definition: def.ToPB(),
 	})
+	if err != nil {
+		return nil, err
+	}
 
-	return builderRes, nil
+	// Read the config file from the result of the nix build and add it to the result metadata
+	configByte, err := builderRef.ReadFile(ctx, client.ReadRequest{Filename: "/" + manifest.Config})
+	if err != nil {
+		return nil, err
+	}
+	res.AddMeta(exptypes.ExporterImageConfigKey, configByte)
+
+	return res, nil
 }
