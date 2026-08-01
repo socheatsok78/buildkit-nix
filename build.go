5513218e10bcbc16d893d0a9cca0e4e1cf741460
Socheat <github@socheat.dev>
2026-08-01 14:06:06 +0700

    [EXPERIMENTAL]: impl multi-platform build
    [EXPERIMENTAL]: attempt to resolve multi-platform baseimage
    
    Revert "[EXPERIMENTAL]: attempt to resolve multi-platform baseimage"
    
    This reverts commit a0d9ec24e67244897176acddd9216c9007586a0c.
    
    Revert "[EXPERIMENTAL]: impl multi-platform build"
    
    This reverts commit 4ab10582a32bd0abb4b2e2927da8d66fb645a0ce.
    
    Reapply "[EXPERIMENTAL]: impl multi-platform build"
    
    This reverts commit f168eaabb99f29596ac56ed98e0b62bc895f7fe1.
    
    Reapply "[EXPERIMENTAL]: attempt to resolve multi-platform baseimage"
    
    This reverts commit 093ce011be8eeba9a322e0738710ff7910cc3398.
    
    [EXPERIMENTAL]: attempt to impl multi-platform build
    
    Revert "[DRAFT]: nix build --impure"
    
    This reverts commit 5b208213fcd152f5b7638d23ae2fafc83b45e6a3.
    
    # Conflicts:
    #	build.go
    
    Update builder output
    
    Add filter-syscalls = false to nix build command
    
    Prettify target name
    
    Remove base image config since we don't have one
    
    Add comments

--- build.go
+++ build.go
@@ -2,12 +2,15 @@ package builder
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/moby/buildkit/client/llb"
-	"github.com/moby/buildkit/exporter/containerimage/exptypes"
+	"github.com/moby/buildkit/frontend/dockerui"
 	"github.com/moby/buildkit/frontend/gateway/client"
+	dockerocispecs "github.com/moby/docker-image-spec/specs-go/v1"
+	ocispecs "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/socheatsok78/buildkit-nix/pkg/dockershim"
 	"github.com/socheatsok78/buildkit-nix/pkg/nixui"
 )
@@ -21,7 +24,12 @@ const (
 )
 
 func Build(ctx context.Context, c client.Client) (*client.Result, error) {
-	opts := c.BuildOpts().Opts
+	c = &withResolveCache{Client: c}
+	bc, err := dockerui.NewClient(c)
+	if err != nil {
+		return nil, err
+	}
+	opts := bc.BuildOpts().Opts
 
 	// also accept build args from Moby
 	for k, v := range opts {
@@ -32,103 +40,139 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 
 	// Check if a target is specified in the build options, Use it for nix build installables
 	target := ""
+	prettyTarget := ""
 	if v, ok := opts[keyTarget]; ok {
 		target = fmt.Sprintf("#%v", v)
+		prettyTarget = fmt.Sprintf(".#%v", target)
 	}
 
 	// Load the source code from the build context
 	src := llb.Local(LocalNameContext, llb.SessionID(c.BuildOpts().SessionID), llb.SharedKeyHint("nix-src"), nixui.WithInternalName("load source from build context"))
 
-	// Load the nix image and set it as the base image for the build
-	builder := llb.Image(NixImage, llb.WithMetaResolver(c), nixui.WithInternalName(fmt.Sprintf("load builder image from %s", NixImage)))
+	// Using the dockerui.Client to enable multi-platform builds
+	// The dockerui.Client will handle the platform selection and build execution for us
+	rb, err := bc.Build(ctx, func(ctx context.Context, platform *ocispecs.Platform, idx int) (*dockerui.BuildResult, error) {
+		withInternalName := nixui.WithInternalName
+		if platform != nil {
+			withInternalName = nixui.WithInternalNameTag(fmt.Sprintf("%s/%s", platform.OS, platform.Architecture))
+		}
 
-	// Configure builder
-	builder = builder.With(
-		llb.Dir("/workspace"),
-	)
+		// Load the nix image and set it as the base image for the build
+		builderOpts := []llb.ImageOption{
+			llb.WithMetaResolver(c),
+			withInternalName(fmt.Sprintf("load builder image from %s", NixImage)),
+		}
+		if platform != nil {
+			builderOpts = append(builderOpts, llb.Platform(*platform))
+		}
+		builder := llb.Image(NixImage, builderOpts...)
 
-	// Run the nix build command inside the nix image
-	builderSt := builder.Run(
-		llb.AddMount("/src", src, llb.Readonly),
-		llb.AddMount("/build", llb.Scratch()),
-		llb.Args([]string{
-			"nix",
-			"--option", "sandbox", "false",
-			"--extra-experimental-features", "nix-command",
-			"--extra-experimental-features", "flakes",
-			"build",
-			"--option", "build-users-group", "",
-			fmt.Sprintf("/src%s", target),
-			"--impure",
-		}),
-	)
+		// Configure builder
+		builder = builder.With(
+			llb.Dir("/workspace"),
+		)
+		// Run the nix build command inside the nix image
+		builderSt := builder.Run(
+			llb.AddMount("/src", src, llb.Readonly),
+			llb.AddMount("/build", llb.Scratch()),
+			llb.Args([]string{
+				"nix",
+				"--option", "sandbox", "false",
+				"--option", "filter-syscalls", "false",
+				"--extra-experimental-features", "nix-command",
+				"--extra-experimental-features", "flakes",
+				"build",
+				"--option", "build-users-group", "",
+				fmt.Sprintf("/src%s", target),
+			}),
+			withInternalName(fmt.Sprintf("nix build %s", prettyTarget)),
+		)
 
-	// Extract the result of the nix build to a new scratch state
-	extract := llb.Scratch()
-	extractSt := extract.File(
-		llb.Copy(builderSt.GetMount("/"), "/workspace/result", "/", &llb.CopyInfo{
-			AttemptUnpack: true,
-		}),
-		nixui.WithInternalName("extracting result layers"),
-	)
-
-	// Prepare the builder state to extract the manifest.json file
-	extractDef, err := extractSt.Marshal(ctx, llb.WithCaps(c.BuildOpts().LLBCaps))
-	if err != nil {
-		return nil, err
-	}
-	extractRes, err := c.Solve(ctx, client.SolveRequest{
-		Definition: extractDef.ToPB(),
-	})
-	if err != nil {
-		return nil, err
-	}
-	extractRef, err := extractRes.SingleRef()
-	if err != nil {
-		return nil, err
-	}
-
-	// Parse the manifest.json file to get the list of layers and the config file
-	manifestByte, err := extractRef.ReadFile(ctx, client.ReadRequest{Filename: "/manifest.json"})
-	if err != nil {
-		return nil, err
-	}
-	manifest, err := dockershim.UnmarshalManifest(manifestByte)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create a new scratch state to hold the extracted layers
-	layered := llb.Scratch()
-
-	// Import each layer from the result of the nix build into the new scratch state
-	for _, layer := range manifest.Layers {
-		layered = layered.File(
-			llb.Copy(extractSt, layer, "/", &llb.CopyInfo{
+		// Extract the result of the nix build to a new scratch state
+		extract := llb.Scratch()
+		extractSt := extract.File(
+			llb.Copy(builderSt.GetMount("/"), "/workspace/result", "/", &llb.CopyInfo{
 				AttemptUnpack: true,
 			}),
-			nixui.WithInternalName(fmt.Sprintf("importing layer: %s", layer)),
+			withInternalName("extracting result layers"),
 		)
-	}
 
-	// Prepare the final definition from final state
-	layeredDef, err := layered.Marshal(ctx)
-	if err != nil {
-		return nil, err
-	}
-	layeredRes, err := c.Solve(ctx, client.SolveRequest{
-		Definition: layeredDef.ToPB(),
+		// Prepare the builder state to extract the manifest.json file
+		extractDef, err := extractSt.Marshal(ctx, llb.WithCaps(c.BuildOpts().LLBCaps))
+		if err != nil {
+			return nil, err
+		}
+		extractRes, err := c.Solve(ctx, client.SolveRequest{
+			Definition: extractDef.ToPB(),
+		})
+		if err != nil {
+			return nil, err
+		}
+		extractRef, err := extractRes.SingleRef()
+		if err != nil {
+			return nil, err
+		}
+
+		// Parse the manifest.json file to get the list of layers and the config file
+		manifestByte, err := extractRef.ReadFile(ctx, client.ReadRequest{Filename: "/manifest.json"})
+		if err != nil {
+			return nil, err
+		}
+		manifest, err := dockershim.UnmarshalManifest(manifestByte)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create a new scratch state to hold the extracted layers
+		layered := llb.Scratch()
+
+		// Import each layer from the result of the nix build into the new scratch state
+		for _, layer := range manifest.Layers {
+			layered = layered.File(
+				llb.Copy(extractSt, layer, "/", &llb.CopyInfo{
+					AttemptUnpack: true,
+				}),
+				withInternalName(fmt.Sprintf("importing layer: %s", layer)),
+			)
+		}
+
+		// Prepare the final definition from final state
+		layeredDef, err := layered.Marshal(ctx)
+		if err != nil {
+			return nil, err
+		}
+		layeredRes, err := c.Solve(ctx, client.SolveRequest{
+			Definition: layeredDef.ToPB(),
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		layeredRef, err := layeredRes.SingleRef()
+		if err != nil {
+			return nil, err
+		}
+
+		// Read the config file from the result of the nix build and add it to the result metadata
+		configByte, err := extractRef.ReadFile(ctx, client.ReadRequest{Filename: "/" + manifest.Config})
+		if err != nil {
+			return nil, err
+		}
+
+		// Parse the oci config file to get the image configuration
+		var config dockerocispecs.DockerOCIImage
+		if err := json.Unmarshal(configByte, &config); err != nil {
+			return nil, err
+		}
+
+		return &dockerui.BuildResult{
+			Reference: layeredRef,
+			Image:     &config,
+		}, nil
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	// Read the config file from the result of the nix build and add it to the result metadata
-	configByte, err := extractRef.ReadFile(ctx, client.ReadRequest{Filename: "/" + manifest.Config})
-	if err != nil {
-		return nil, err
-	}
-	layeredRes.AddMeta(exptypes.ExporterImageConfigKey, configByte)
-
-	return layeredRes, nil
+	return rb.Finalize()
 }
--- pkg/nixui/nixui.go
+++ pkg/nixui/nixui.go
@@ -5,3 +5,9 @@ import "github.com/moby/buildkit/client/llb"
 func WithInternalName(name string) llb.ConstraintsOpt {
 	return llb.WithCustomName("[nix] " + name)
 }
+
+func WithInternalNameTag(suffix string) func(name string) llb.ConstraintsOpt {
+	return func(name string) llb.ConstraintsOpt {
+		return llb.WithCustomNamef("[nix %s] %s", suffix, name)
+	}
+}
