1c1881204a581f6603c93953ae2dc153194670b2
Socheat <github@socheat.dev>
2026-08-01 19:02:00 +0700

    Add labels to buildkit-nix-image

--- default.nix
+++ default.nix
@@ -26,6 +26,15 @@ rec {
     contents = [ buildkit-nix ];
     config = {
       Cmd = [ "${buildkit-nix}/bin/nix-frontend" ];
+      Labels = {
+        "moby.buildkit.frontend.network.none" = "true";
+
+        # nix-frontend isn't technically support these capabilities,
+        # This is a workaround for the following error:
+        # - buildx bake failed with: ERROR: current frontend does not support defining additional contexts for targets.
+        #   Named contexts are supported since Dockerfile v1.4. Use #syntax directive in Dockerfile or update to latest BuildKit.
+        "moby.buildkit.frontend.caps" = "moby.buildkit.frontend.inputs,moby.buildkit.frontend.contexts";
+      };
     };
   };
 
