034ddfabe3c13cae022a6cd841febb700953fd75
Socheat <github@socheat.dev>
2026-08-03 12:00:30 +0700

    Rework fmt.Sprintf call to use llb.Shlexf

--- build.go
+++ build.go
@@ -113,7 +113,7 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		nixStoreRestoreOpts := []llb.RunOption{
 			llb.AddEnv(keyNixSessionID, c.BuildOpts().SessionID),
 			llb.AddMount(mountNixStoreCacheDir, nixStore, llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
-			llb.Shlex(fmt.Sprintf("cp -anfT %s /nix", mountNixStoreCacheDir)),
+			llb.Shlexf("cp -anfT %s /nix", mountNixStoreCacheDir),
 			withInternalName("configure nix store"),
 		}
 		if bc.IsNoCache("nix-store") {
@@ -151,7 +151,7 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		nixStoreSaveOpts := []llb.RunOption{
 			llb.AddEnv(keyNixSessionID, c.BuildOpts().SessionID),
 			llb.AddMount(mountNixStoreCacheDir, nixStore, llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
-			llb.Shlex(fmt.Sprintf("cp -anfT /nix %s", mountNixStoreCacheDir)),
+			llb.Shlexf("cp -anfT /nix %s", mountNixStoreCacheDir),
 			withInternalName("create nix store snapshot"),
 		}
 		if bc.IsNoCache("nix-store") {
