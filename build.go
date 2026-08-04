ddd75d0fe81c0ff51fc87b8b84fbba1fa8f3768d
Socheat <github@socheat.dev>
2026-08-04 10:54:28 +0700

    Adjust nix store snapshot

--- build.go
+++ build.go
@@ -195,7 +195,7 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		nixStoreSaveOpts := []llb.RunOption{
 			llb.AddEnv(keyNixSessionID, c.BuildOpts().SessionID),
 			llb.AddMount(mountNixStoreCacheDir, nixStore, llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
-			llb.Shlexf("cp -anT /nix %s", mountNixStoreCacheDir),
+			llb.Shlexf("cp -afT /nix %s", mountNixStoreCacheDir),
 			withInternalName("create nix store snapshot"),
 		}
 		if bc.IsNoCache("nix-store") {
