1f8acfaf762ac49491025c313add3bd339647d2c
Socheat <github@socheat.dev>
2026-08-05 21:14:51 +0700

    Refactor build instructions and temporary disable nix store cache

--- build.go
+++ build.go
@@ -61,11 +61,6 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		NixImage = v
 	}
 
-	source := ""
-	if v, ok := opts[keySource]; ok {
-		source = v
-	}
-
 	// If no target is specified, set to "default"
 	if bc.Target == "" {
 		bc.Target = "default"
@@ -105,15 +100,12 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		if err != nil {
 			return nil, err
 		}
-		nixStoreCacheKey := path.Clean(fmt.Sprintf("%s/%s/%s/%s/nix/store", bc.CacheIDNamespace, nixImageDigest, source, bc.Target))
+		nixStoreCacheKey := path.Clean(fmt.Sprintf("%s/%s/%s/nix/store", bc.CacheIDNamespace, nixImageDigest, bc.Target))
 
 		withInternalName := nixui.WithInternalName
 		if bc.MultiPlatformRequested {
 			withInternalName = nixui.WithInternalNameTag(fmt.Sprintf("%s/%s", p.OS, p.Architecture))
 		}
-
-		// Nix store is used to persist the nix store between builds, so that we don't have to rebuild everything from scratch every time
-		nixStore := llb.Scratch()
 
 		// Load the nix image and set it as the base image for the build
 		builderImageOpts := []llb.ImageOption{
@@ -127,72 +119,38 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		}
 		builder := llb.Image(NixImage, builderImageOpts...)
 
-		// Builder working directory is set to /mnt/workspace,
-		// so that the nix build result can be copied to /mnt/workspace/result and extracted later
-		builder = builder.With(llb.Dir(mountWorkspaceDir))
-
-		// setup build environment
-		builder = builder.Run(
-			llb.AddMount(mountSourceDir, *mainContext, llb.Readonly),
+		// Nix build
+		nixBuildOpts := []llb.RunOption{
+			llb.AddEnv("BUILDKIT_NIX_STORE_CACHE_KEY", nixStoreCacheKey),
+			llb.AddEnv("BUILDKIT_NIX_BUILD_TARGET", bc.Target),
+			// llb.AddMount("/nix", builder, llb.SourcePath("/nix"), llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
+			llb.AddMount("/build", llb.Scratch()),
+			llb.AddMount(mountSourceDir, *mainContext),
+			llb.Dir(mountSourceDir),
 			nixllb.Shlexf(`
+				set -euo pipefail
+
+				mkdir -p /mnt/workspace
+
+				echo "Setup build environment"
 				nix --version
 				{
 					echo "auto-optimise-store = true"
 					echo "binary-caches-parallel-connections = 15"
 					echo "extra-experimental-features = flakes"
 					echo "extra-experimental-features = nix-command"
+					echo "extra-experimental-features = configurable-impure-env"
 					echo "filter-syscalls = false"
 					# echo "sandbox = false" -- already set by default from nixos/nix image
-				} >> /etc/nix/nix.conf
-				cat /etc/nix/nix.conf
+				} | tee -a /etc/nix/nix.conf
 
 				# This is a fake config for debugging purposes,
 				# it will be printed in the build logs, but it will not be used by nix
-				echo "nix-store-cache-key = %s"
-			`, nixStoreCacheKey),
-			withInternalName("setup build environment"),
-		).Root()
-
-		// restore nix store cache
-		nixStoreRestoreOpts := []llb.RunOption{
-			llb.AddMount(mountSourceDir, *mainContext, llb.Readonly),
-			llb.AddMount(mountNixStoreCacheDir, nixStore, llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
-			nixllb.Shlexf(`
-				export NIX_STORE_CACHE_DIR="%s"
-				ls /nix/store > /tmp/nix-store-before.txt
-				if [ -d "${NIX_STORE_CACHE_DIR}/var" ]; then
-					cp -afT "${NIX_STORE_CACHE_DIR}/var" /nix/var
-				fi
-				if [ -d "${NIX_STORE_CACHE_DIR}/store" ]; then
-					for f in "${NIX_STORE_CACHE_DIR}/store"/*; do
-						if ! grep -q "$(basename "$f")" /tmp/nix-store-before.txt; then
-							echo "copying path \"$f\" from nix store snapshot..."
-							cp -afT "$f" "/nix/store/$(basename "$f")"
-						fi
-					done
-					ls /nix/store > /tmp/nix-store-before.txt
-				fi
-			`, mountNixStoreCacheDir),
-			withInternalName("configure nix store"),
-		}
-		if bc.IsNoCache("nix-store") {
-			nixStoreRestoreOpts = append(nixStoreRestoreOpts, llb.IgnoreCache)
-		}
-		builder = builder.Run(nixStoreRestoreOpts...).Root()
-
-		// Nix build
-		nixBuildOpts := []llb.RunOption{
-			llb.AddMount(mountSourceDir, *mainContext, llb.Readonly),
-			llb.AddMount(mountNixStoreCacheDir, nixStore, llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
-			llb.AddMount("/build", llb.Scratch()),
-			nixllb.Shlexf(`
-				set -euo pipefail
+				echo "nix-store-cache-key = ${BUILDKIT_NIX_STORE_CACHE_KEY}"
+				echo ""
 
 				nixopts=()
-				installable="%s#%s"
-
 				echo "Prepare build environment..."
-				echo "extra-experimental-features = configurable-impure-env" >> /etc/nix/nix.conf
 
 				# If GITHUB_TOKEN is not empty, then add it to the nix options as a secret
 				if [ -n "${GITHUB_TOKEN:-}" ]; then
@@ -210,7 +168,7 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 				done
 
 				echo -e "\nBuild log data will stream in below:"
-				nix "${nixopts[@]}" --show-trace --log-format raw build "$installable"
+				nix "${nixopts[@]}" --show-trace --log-format raw build ".#${BUILDKIT_NIX_BUILD_TARGET}"
 				echo -e "\nBuild finished!"
 
 				echo -e "\nPerforming post-build checks:\n"
@@ -227,10 +185,13 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 					echo "  See https://nix.dev/tutorials/nixos/building-and-running-docker-images.html for more information on how to build Docker images with Nix."
 					exit 1
 				fi
+
 				echo "-  Found Docker-compatible repository tarball, proceeding with extraction of layers and config file."
+				echo "-  Copying the result of the nix build to the workspace directory for further processing..."
+				cp -R $(nix-store -Rq result/) /mnt/workspace/result
 				echo ""
 				exit 0
-			`, mountSourceDir, bc.Target),
+			`),
 			withInternalName(fmt.Sprintf("nix build .#%s", bc.Target)),
 		}
 		for _, secret := range nixBuildSecrets {
@@ -240,28 +201,6 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 			nixBuildOpts = append(nixBuildOpts, llb.IgnoreCache)
 		}
 		builder = builder.Run(nixBuildOpts...).Root()
-
-		// save nix store cache
-		nixStoreSaveOpts := []llb.RunOption{
-			llb.AddMount(mountSourceDir, *mainContext, llb.Readonly),
-			llb.AddMount(mountNixStoreCacheDir, nixStore, llb.AsPersistentCacheDir(nixStoreCacheKey, llb.CacheMountLocked)),
-			nixllb.Shlexf(`
-				export NIX_STORE_CACHE_DIR="%s"
-				mkdir -p "${NIX_STORE_CACHE_DIR}/store" "${NIX_STORE_CACHE_DIR}/var"
-				cp -afT /nix/var "${NIX_STORE_CACHE_DIR}/var"
-				for f in /nix/store/*; do
-					if ! grep -q "$(basename "$f")" /tmp/nix-store-before.txt; then
-						echo "copying path \"$f\" to nix store snapshot..."
-						cp -afT "$f" "${NIX_STORE_CACHE_DIR}/store/$(basename "$f")"
-					fi
-				done
-			`, mountNixStoreCacheDir),
-			withInternalName("create nix store snapshot"),
-		}
-		if bc.IsNoCache("nix-store") {
-			nixStoreSaveOpts = append(nixStoreSaveOpts, llb.IgnoreCache)
-		}
-		builder = builder.Run(nixStoreSaveOpts...).Root()
 
 		// Extract the result of the nix build to a new scratch state
 		extract := llb.Scratch()
