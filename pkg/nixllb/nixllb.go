5e57a8b88e4a56017075f27659cf88408ef9e6a8
Socheat <github@socheat.dev>
2026-08-15 00:32:26 +0700

    impl `nixllb.ProgressGroup` func

--- pkg/nixllb/nixllb.go
+++ pkg/nixllb/nixllb.go
@@ -1,1 +1,11 @@
 package nixllb
+
+import (
+	"fmt"
+
+	"github.com/moby/buildkit/client/llb"
+)
+
+func ProgressGroup(id string, name string, weak bool) llb.ConstraintsOpt {
+	return llb.ProgressGroup(id, fmt.Sprintf("[nix] %s", name), weak)
+}
