7b1f0adf9eb0d36bfc268cdf1e5a2923ea6a8037
Socheat <github@socheat.dev>
2026-08-19 21:32:04 +0700

    update pkg/nixui/nixui.go

--- pkg/nixui/nixui.go
+++ pkg/nixui/nixui.go
@@ -1,6 +1,8 @@
 package nixui
 
-import "github.com/moby/buildkit/client/llb"
+import (
+	"github.com/moby/buildkit/client/llb"
+)
 
 func WithInternalName(name string) llb.ConstraintsOpt {
 	return llb.WithCustomName("[nix] " + name)
