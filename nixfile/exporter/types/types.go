dd429fd74ffc6c4dbefde335850eb2015ce2cc03
Socheat <github@socheat.dev>
2026-08-20 11:20:05 +0700

    update ExportConfig struct

--- nixfile/exporter/types/types.go
+++ nixfile/exporter/types/types.go
@@ -2,14 +2,16 @@ package types
 
 import (
 	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/frontend/gateway/client"
 	dockerocispec "github.com/moby/docker-image-spec/specs-go/v1"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 type ExportConfig struct {
-	State       llb.State
-	Platform    ocispec.Platform
-	IgnoreCache bool
+	State        llb.State
+	Platform     ocispec.Platform
+	IgnoreCache  bool
+	CacheImports []client.CacheOptionsEntry
 }
 
 type ExportResult struct {
