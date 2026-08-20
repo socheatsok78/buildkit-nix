e5dd251126dfd392779d8fe49e30f614263c627d
Socheat <github@socheat.dev>
2026-08-20 10:47:52 +0700

    cleanup exporter codes

--- nixfile/exporter/exporter.go
+++ nixfile/exporter/exporter.go
@@ -1,10 +1,46 @@
 package exporter
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	"github.com/moby/buildkit/client/llb"
-	"github.com/socheatsok78/buildkit-nix/pkg/nixui"
+	"github.com/moby/buildkit/frontend/gateway/client"
+	"github.com/socheatsok78/buildkit-nix/nixfile/exporter/plugins"
+	"github.com/socheatsok78/buildkit-nix/nixfile/exporter/types"
 )
 
-func withInternalNameW(name string) llb.ConstraintsOpt {
-	return nixui.WithInternalName(name)
+func Export(ctx context.Context, c client.Client, cfg types.ExportConfig) (types.ExportResult, error) {
+	def, err := cfg.State.Marshal(ctx, llb.WithCaps(c.BuildOpts().LLBCaps))
+	if err != nil {
+		return types.ExportResult{}, err
+	}
+
+	res, err := c.Solve(ctx, client.SolveRequest{
+		Definition: def.ToPB(),
+	})
+	if err != nil {
+		return types.ExportResult{}, err
+	}
+
+	ref, err := res.SingleRef()
+	if err != nil {
+		return types.ExportResult{}, err
+	}
+
+	resultTypeByte, err := ref.ReadFile(ctx, client.ReadRequest{Filename: "/type"})
+	if err != nil {
+		return types.ExportResult{}, err
+	}
+	resultType := strings.TrimSpace(string(resultTypeByte))
+
+	// Use the result type to select the appropriate plugin for exporting
+	plugin, ok := plugins.GetPlugin(resultType)
+	if !ok {
+		return types.ExportResult{}, fmt.Errorf("no plugin found for result type: %s", resultType)
+	}
+
+	// Call the plugin's Export method to handle the export process
+	return plugin.Export(ctx, c, cfg)
 }
--- nixfile/exporter/types/types.go
+++ nixfile/exporter/types/types.go
@@ -1,18 +1,10 @@
 package types
 
 import (
-	"context"
-
 	"github.com/moby/buildkit/client/llb"
-	"github.com/moby/buildkit/frontend/gateway/client"
 	dockerocispec "github.com/moby/docker-image-spec/specs-go/v1"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
-
-type Exporter struct {
-	Ctx    context.Context
-	Client client.Client
-}
 
 type ExportConfig struct {
 	State       llb.State
