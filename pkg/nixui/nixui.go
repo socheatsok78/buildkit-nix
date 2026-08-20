a9089ee714cda9f1ae7e85a0bb67c554507d0f81
Socheat <github@socheat.dev>
2026-08-20 17:08:39 +0700

    update pkg/nixui/nixui.go

--- pkg/nixui/nixui.go
+++ pkg/nixui/nixui.go
@@ -12,8 +12,8 @@ func WithInternalNamef(format string, args ...interface{}) llb.ConstraintsOpt {
 	return llb.WithCustomNamef("[nix "+format+"]", args...)
 }
 
-func WithInternalNameTag(suffix string) func(name string) llb.ConstraintsOpt {
+func WithInternalNameTag(tag string) func(name string) llb.ConstraintsOpt {
 	return func(name string) llb.ConstraintsOpt {
-		return llb.WithCustomNamef("[nix %s] %s", suffix, name)
+		return llb.WithCustomNamef("[nix %s] %s", tag, name)
 	}
 }
